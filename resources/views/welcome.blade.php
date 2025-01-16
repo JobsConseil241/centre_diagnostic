bcb2d927fb5f258fb6bf27308322b834def6c949
JobsConseil241 <jobsconseil241@gmail.com>
2025-01-16 18:24:36 +0100

    Update link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -215,7 +215,7 @@
 
         @foreach ($market as $key => $image)
             .carousel-item:nth-child({{ $key + 1 }}) {
-                background-image: url('/public/{{ Storage::url($image->thumb_url) }}');
+                background-image: url('/public{{ Storage::url($image->thumb_url) }}');
                 background-repeat: no-repeat;
                 background-size: 100% auto;
                 background-position: center;
